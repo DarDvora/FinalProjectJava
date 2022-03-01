ac1fe5a060c99331c31803313ec2c57f8baef826
DarDvora <dvorah.yahel@gmail.com>
2022-03-01 11:53:52 +0200

    validation

--- src/main/java/CoinValues/ConvertMain.java
+++ src/main/java/CoinValues/ConvertMain.java
@@ -12,35 +12,51 @@ public class ConvertMain {
         boolean b = true;
         int i = 0;
         ArrayList<Double> ConvertHistory = new ArrayList<Double>();
+
+
         while (b=true){
             //Welcome screen
             System.out.println("Welcome to currency converter");
             System.out.println("Please choose an option (1/2):");
             System.out.println("1.Dollars to Shekels");
             System.out.println("2.Shekels to Dollars");
-            Scanner scanner = new Scanner(System.in);
-            int choice = scanner.nextInt();
 
-            //input amount screen
-            System.out.println("Please enter an amount to convert");
-            Scanner scanner1 = new Scanner(System.in);
-            double input = scanner1.nextDouble();
+            // try = non int validation
+            try {
+                Scanner scanner = new Scanner(System.in);
+                int choice = scanner.nextInt();
 
-            //Convert Dollar to Shekel
-            if (choice == 1) {
-                Coin ilsValue = CoinsFactory.getCoinInstance(Coins.ILS);
-                double value = ilsValue.calculate(input);
-                System.out.println("The result is: " + value + " Shekels");
-                ConvertHistory.add(value);
+                // 1/2 validation
+                if (choice != 1 && choice !=2){
+                    throw new Exception("Invalid input");
+                }
+
+                //input amount screen
+                System.out.println("Please enter an amount to convert");
+                Scanner scanner1 = new Scanner(System.in);
+                double input = scanner1.nextDouble();
+
+                //Convert Dollar to Shekel
+                if (choice == 1) {
+                    Coin ilsValue = CoinsFactory.getCoinInstance(Coins.ILS);
+                    double value = ilsValue.calculate(input);
+                    System.out.println("The result is: " + value + " Shekels");
+                    ConvertHistory.add(value);
+                }
+                //Convert Shekel to Dollar
+                else if (choice == 2) {
+                    Coin usdValue = CoinsFactory.getCoinInstance(Coins.USD);
+                    double value = usdValue.calculate(input);
+                    //Result screen
+                    System.out.println("The result is: " + value + " Dollars");
+                    ConvertHistory.add(value);
+                }
+
+                //other input is not valid
+            }catch (Exception e){
+                System.out.println("Invalid input");
             }
-            //Convert Shekel to Dollar
-            else if (choice == 2) {
-                Coin usdValue = CoinsFactory.getCoinInstance(Coins.USD);
-                double value = usdValue.calculate(input);
-                //Result screen
-                System.out.println("The result is: " + value + " Dollars");
-                ConvertHistory.add(value);
-            }
+
             //Start over or exit
             System.out.println("Do you want to start over? enter y/n");
             Scanner scanner2 = new Scanner(System.in);
